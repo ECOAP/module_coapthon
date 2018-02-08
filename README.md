42ff534b0f683f4252118131f049e27a2f3fb63e
Carlo Vallati <warner83@gmail.com>
2018-02-08 16:26:00 +0100

    Changed README

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-WiSHFUL iperf Driver
+WiSHFUL CoAPthon Driver
 ============================
 
 This is a prototypic implementation of a wireless agent providing the Unified
--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ def readme():
         return f.read()
 
 setup(
-    name='wishful_module_iperf',
+    name='wishful_module_coapthon',
     version='0.1.0',
     packages=find_packages(),
     url='http://www.wishful-project.eu/software',
     license='',
-    author='Piotr Gawlowicz',
-    author_email='gawlowicz@tu-berlin.de',
-    description='WiSHFUL Module iperf',
-    long_description='WiSHFUL Module iperf',
-    keywords='wireless control',
+    author='Carlo Vallati',
+    author_email='carlo.vallati@unipi.it',
+    description='WiSHFUL Module Coapthon',
+    long_description='WiSHFUL Module oapthon',
+    keywords='traffic generation',
     install_requires=[]
 )
